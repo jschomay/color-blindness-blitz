1d9141b0d3a768ebc1da81fb863b6fda60654940
Jeff Schomay <jschomay@gmail.com>
2014-07-11 21:25:50 +0100

    it's raining blocks!

--- app/states/play.js
+++ app/states/play.js
@@ -1,62 +1,34 @@
+BLOCKSIZE = 32;
+
 module.exports = GameState = function(game) {
 };
 
 // Load images and sounds
 GameState.prototype.preload = function() {
-    this.game.load.image('scaffold', '/assets/scaffold.png');
-    this.game.load.image('player', '/assets/player.png');
+    this.game.load.image('block', '/assets/block.png');
 };
 
-// Setup the example
 GameState.prototype.create = function() {
-    // Set stage background to something sky colored
-    this.game.stage.backgroundColor = 0x4488cc;
+    this.game.stage.backgroundColor = '#aaa';
 
-    this.game.camera.bounds = null;
-
-    this.GRAVITY = 2600; // pixels/second/second
+    this.GRAVITY = 300; // pixels/second/second
     this.game.physics.arcade.gravity.y = this.GRAVITY;
 
+    this.blockPool = this.game.add.group();
 
-    this.scaffoldPool = this.game.add.group();
-
-    // start scaffold pool with 100 pieces
+    // start block pool with 100 pieces
     for(var i = 0; i < 100; i++) {
-        // addScaffoldToPool.call(this);
-        this.addScaffoldToPool();
+        this.addBlockToPool();
     }
 
-    this.getScaffoldHeight = function () {return this.scaffoldPool.getAt(0).height;};
-    this.getScaffoldWidth = function () {return this.scaffoldPool.getAt(0).width;};
-
-    var Player = require('../entities/player');
-    this.player = new Player(this.game, this.game.width/2, this.game.height/2 - 60);
-    this.game.add.existing(this.player);
-
-
-    // invisible helper object to determine if scaffolding exists in the direction pressed
-    this.targetPosition = this.game.add.sprite(this.player.x, this.player.y);
-    this.game.physics.enable(this.targetPosition, Phaser.Physics.ARCADE);
-    this.targetPosition.body.allowGravity = false;
-    this.targetPosition.body.setSize(10, this.getScaffoldHeight()/2, this.targetPosition.width/2-5, this.targetPosition.height/2-5);
-
-
-    // lay some initial scaffolding
-    for(var x = 0; x < this.game.width; x += this.getScaffoldWidth()) {
-        this.buildScaffold(x, this.game.height/2 - this.getScaffoldHeight()/2);
+    // lay ground
+    for(var x = 0; x < this.game.width; x += BLOCKSIZE) {
+        var block = this.placeBlock(x, this.game.height - BLOCKSIZE);
+        block.body.immovable = true;
+        block.body.allowGravity = false;
     }
 
-
-    // Capture certain keys to prevent their default actions in the browser.
-    // This is only necessary because this is an HTML5 game. Games on other
-    // platforms may not need code like this.
-    this.game.input.keyboard.addKeyCapture([
-        Phaser.Keyboard.LEFT,
-        Phaser.Keyboard.RIGHT,
-        Phaser.Keyboard.UP,
-        Phaser.Keyboard.DOWN
-    ]);
-
+    this.rainBlocks();
 
     // Show FPS
     this.game.time.advancedTiming = true;
@@ -65,72 +37,44 @@ GameState.prototype.create = function() {
     );
 };
 
-GameState.prototype.addScaffoldToPool = function(){
-    var scaffold = this.game.add.sprite(0, 0, 'scaffold');
-    scaffold.name = 'scaffold'+this.scaffoldPool.length;
-    scaffold.scale.x = 2.5;
-    scaffold.scale.y = 2;
-    this.scaffoldPool.add(scaffold);
+GameState.prototype.rainBlocks = function() {
+    var slots = this.game.width/BLOCKSIZE;
+    var x = (this.game.rnd.integerInRange(0,slots)) * BLOCKSIZE;
+    this.placeBlock(x, -BLOCKSIZE);
+    this.game.time.events.add(1000, this.rainBlocks, this);
+};
 
-    // Enable physics on the scaffold
-    this.game.physics.enable(scaffold, Phaser.Physics.ARCADE);
-    scaffold.body.immovable = true;
-    scaffold.body.allowGravity = false;
-    scaffold.body.setSize(32, 5, 0 , 64-10);
+GameState.prototype.addBlockToPool = function(){
+    var block = this.game.add.sprite(0, 0, 'block');
+    block.name = 'block'+this.blockPool.length;
+    block.scale.x = 1;
+    block.scale.y = 1;
+    this.blockPool.add(block);
+
+    // Enable physics on the block
+    this.game.physics.enable(block, Phaser.Physics.ARCADE);
 
     // Set its initial state to "dead".
-    scaffold.kill();
+    block.kill();
 
-    scaffold.checkWorldBounds = true;
-    scaffold.outOfBoundsKill = true;
+    block.body.bounce.y = 0.1;
+    block.body.checkWorldBounds = true;
+    block.body.outOfBoundsKill = true;
 
-    return scaffold;
+    return block;
 };
 
-GameState.prototype.buildScaffold = function (x, y) {
-    var buildTime = Phaser.Timer.SECOND * 0.6;
-    this.player.building = true;
-    // Get a dead scaffold from the pool
-    var scaffold = this.scaffoldPool.getFirstDead();
-    if (scaffold === null) {
-        console.log("increasing scaffold pool to", this.scaffoldPool.length);
-        scaffold = this.addScaffoldToPool();
+GameState.prototype.placeBlock = function (x, y) {
+    // Get a dead block from the pool
+    var block = this.blockPool.getFirstDead();
+    if (block === null) {
+        console.log("increasing block pool to", this.blockPool.length);
+        block = this.addBlockToPool();
     }
-    scaffold.revive();
-    scaffold.reset(x, y);
-    scaffold.alpha = 0;
-    // fade in (temp animation)
-    this.game.add.tween(scaffold).to( { alpha: 1 }, buildTime, Phaser.Easing.Linear.None, true);
+    block.revive();
+    block.reset(x, y);
 
-    function stopBuilding() {this.player.building = false;}
-    this.game.time.events.add(buildTime, stopBuilding, this);
-
-    return scaffold;
-};
-
-GameState.prototype.extendScaffold = function(anchorScaffold, direction) {
-    // TODO check for anchorScaffold, maybe here, or maybe in function that calls?
-    switch (direction) {
-        case 'up':
-        this.buildScaffold(anchorScaffold.x, anchorScaffold.y - anchorScaffold.height);
-        break;
-        case 'right':
-        this.buildScaffold(anchorScaffold.x + anchorScaffold.width, anchorScaffold.y);
-        break;
-        case 'left':
-        this.buildScaffold(anchorScaffold.x - anchorScaffold.width, anchorScaffold.y);
-        break;
-    }
-};
-
-GameState.prototype.getScaffoldUnderfoot = function() {
-    var scaffoldUnderFoot;
-    this.targetPosition.body.reset(this.player.x, this.player.y + this.player.height/2 + 5);
-    function collisionHandler(player, scaffold) {
-        scaffoldUnderFoot = scaffold;
-    }
-    this.game.physics.arcade.overlap(this.targetPosition, this.scaffoldPool, collisionHandler, null, this);
-    return scaffoldUnderFoot;
+    return block;
 };
 
 
@@ -140,7 +84,7 @@ GameState.prototype.getScaffoldUnderfoot = function() {
 //     var bitmap = this.game.add.bitmapData(this.game.width, this.game.height);
 
 //     // These functions use the canvas context to draw lines using the canvas API
-//     for(y = this.game.height-32; y >= 64; y -= 32) {
+//     for(y = this.game.height-BLOCKSIZE; y >= 64; y -= BLOCKSIZE) {
 //         bitmap.context.beginPath();
 //         bitmap.context.strokeStyle = 'rgba(255, 255, 255, 0.2)';
 //         bitmap.context.moveTo(0, y);
@@ -151,108 +95,31 @@ GameState.prototype.getScaffoldUnderfoot = function() {
 //     this.game.add.image(0, 0, bitmap);
 // };
 
-// The update() method is called every frame
+// TODO move this into block.js
+onCollide = function(block1, block2){
+    // hacky way to let a block bounce once then stop
+    // gets around bug(?) where stacked blocks bounce slightly
+    // and fall through when stacks get to "heavy"
+    if (block2.bounced) {
+        block2.body.velocity.y = 0;
+        block2.body.immovable = true;
+        block2.body.allowGravity = false;
+    } else {
+        block2.bounced = true;
+    }
+};
+
 GameState.prototype.update = function() {
     if (this.game.time.fps !== 0) {
         this.fpsText.setText(this.game.time.fps + ' FPS');
     }
+    this.game.physics.arcade.collide(this.blockPool, this.blockPool, onCollide);
 
-    // Collide the player with the scaffold
-    this.game.physics.arcade.collide(this.player, this.scaffoldPool);
-
-    var playersFeet = this.player.y+this.player.height/2 + 5;
-
-    // targetPosition follows player
-    this.targetPosition.body.reset(this.player.x, playersFeet);
-
-    this.player.body.acceleration.x = 0;
-
-    // update inputs
-
-    if (!this.player.isBusy() && this.leftInputIsActive()) {
-        this.targetPosition.body.reset(this.player.x - this.getScaffoldWidth()*4/5, playersFeet);
-        this.buildOrMove('left', this.player.walkLeft);
-    }
-    if (!this.player.isBusy() && this.rightInputIsActive()) {
-        this.targetPosition.body.reset(this.player.x + this.getScaffoldWidth()*4/5, playersFeet);
-        this.buildOrMove('right', this.player.walkRight);
-    }
-    if (!this.player.isBusy() && this.upInputIsActive()) {
-        this.player.body.acceleration.x = 0;
-        this.targetPosition.body.reset(this.player.x, playersFeet - this.getScaffoldHeight());
-        this.buildOrMove('up', this.player.climb);
-    }
-
-    // for debugging actions //
-    if(this.input.keyboard.justPressed(Phaser.Keyboard.DOWN,1)){
-        this.getScaffoldUnderfoot();
-    }
 };
 
-GameState.prototype.buildOrMove = function(direction, moveFn) {
-    // Collide the targetPosition with the scaffold
-    if(this.game.physics.arcade.overlap(this.targetPosition, this.scaffoldPool)) {
-        // scaffold exists to move to
-        // TODO - only climb if up is held for .5 sec
-        moveFn.call(this.player, this);
-    } else {
-        // stop and build
-        this.player.body.acceleration.x = 0;
-
-        // direction passed in as 'up', 'left', 'right'
-        anchorScaffold = this.getScaffoldUnderfoot();
-        this.extendScaffold(anchorScaffold, direction);
-
-        // TODO - DON'T BUILD IF IT WILL BE OFF SCREEN
-    }
-};
-
-// This function should return true when the player activates the "go left" control
-// In this case, either holding the right arrow or tapping or clicking on the left
-// side of the screen.
-GameState.prototype.leftInputIsActive = function() {
-    var isActive = false;
-
-    isActive = this.input.keyboard.isDown(Phaser.Keyboard.LEFT);
-    isActive |= (this.game.input.activePointer.isDown &&
-        this.game.input.activePointer.x < this.game.width/4);
-
-    return isActive;
-};
-
-// This function should return true when the player activates the "go right" control
-// In this case, either holding the right arrow or tapping or clicking on the right
-// side of the screen.
-GameState.prototype.rightInputIsActive = function() {
-    var isActive = false;
-
-    isActive = this.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
-    isActive |= (this.game.input.activePointer.isDown &&
-        this.game.input.activePointer.x > this.game.width/2 + this.game.width/4);
-
-    return isActive;
-};
-
-// This function should return true when the player activates the "jump" control
-// In this case, either holding the up arrow or tapping or clicking on the center
-// part of the screen.
-GameState.prototype.upInputIsActive = function(duration) {
-    var isActive = false;
-
-    isActive = this.input.keyboard.justPressed(Phaser.Keyboard.UP, duration);
-    isActive |= (this.game.input.activePointer.justPressed(duration + 1000/60) &&
-        this.game.input.activePointer.x > this.game.width/4 &&
-        this.game.input.activePointer.x < this.game.width/2 + this.game.width/4);
-
-    return isActive;
-};
 
 GameState.prototype.render = function render() {
 
-    // game.debug.bodyInfo(this.player, 32, 32);
 
-    this.game.debug.body(this.targetPosition);
-    this.game.debug.body(this.player);
-    // this.game.debug.spriteBounds(this.player);
-    this.scaffoldPool.forEach(function(scaffold){this.game.debug.body(scaffold);},this);
+    // this.blockPool.forEach(function(block){this.game.debug.body(block);},this);
 };
