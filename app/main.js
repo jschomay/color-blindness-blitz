e20eab63ad2ce57ac8f3879da1d1e8da95dfc913
Jeff Schomay <jschomay@gmail.com>
2014-10-13 08:23:27 +0100

    add intro screen

--- app/main.js
+++ app/main.js
@@ -1,6 +1,8 @@
 var game = new Phaser.Game(320, 480, Phaser.AUTO, 'game');
 // var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'game');
 GameState = require('./states/play');
-game.state.add('game', GameState, true);
+Intro = require('./states/intro');
+game.state.add('intro', Intro, true);
+game.state.add('play', GameState);
 
 window.game = game;
---
+++ app/states/intro.js
@@ -0,0 +1,22 @@
+  function Intro() {}
+
+  Intro.prototype = {
+    preload: function() {
+
+    },
+    create: function() {
+      var style = { font: 'bold 45px Arial', fill: '#ffffff', align: 'center'};
+      this.titleText = this.game.add.text(this.game.world.centerX, 100, 'Color\nBlindness\nBlitz', style);
+      this.titleText.anchor.setTo(0.5, 0.5);
+
+      this.instructionsText = game.add.text(this.game.world.centerX, 300, 'Rules:  When a word lights up, tap any\nword that says the name of the color of\nthe lit up word.  If you get it right in time,\nthe word will clear.  If you miss, it will \n"freeze" onto the screen.  Try to clear all\nthe words.\n\nTap anywhere to start.', { font: '16px Arial', fill: '#ffffff', align: 'left'});
+      this.instructionsText.anchor.setTo(0.5, 0.5);
+    },
+    update: function() {
+      if(this.game.input.activePointer.justPressed()) {
+        this.game.state.start('play');
+      }
+    }
+  };
+
+  module.exports = Intro
