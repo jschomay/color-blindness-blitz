e15fdcf4a0c7d323f8acf7a41d32d65c79a85fa7
Jeff Schomay <jschomay@gmail.com>
2015-01-18 20:54:52 +0000

    improve instructions

--- app/states/intro.js
+++ app/states/intro.js
@@ -9,7 +9,7 @@
       this.titleText = this.game.add.text(this.game.world.centerX, 100, 'Color\nBlindness\nBlitz', style);
       this.titleText.anchor.setTo(0.5, 0.5);
 
-      this.instructionsText = game.add.text(this.game.world.centerX, 300, 'Rules:  When a word lights up, tap any\nword that says the name of the color of\nthe lit up word.  If you get it right in time,\nthe word will clear.  If you miss, it will \n"freeze" onto the screen.  Try to clear all\nthe words.\n\nTap anywhere to start.', { font: '16px Arial', fill: '#ffffff', align: 'left'});
+      this.instructionsText = game.add.text(this.game.world.centerX, 300, 'Rules:  Tap a word that matches the\ncolor of the lit up word.  If you get\nit right in time, the word will clear.\n\nTry to clear all the words.\n\nTap anywhere to start.', { font: '16px Arial', fill: '#ffffff', align: 'left'});
       this.instructionsText.anchor.setTo(0.5, 0.5);
     },
     update: function() {
