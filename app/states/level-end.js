2cd8da04e466ed9391ff4fc2ee39ed042b1be068
Jeff Schomay <jschomay@gmail.com>
2015-01-23 08:22:38 +0000

    add beta version notice and link for feedback and updates

--- app/states/level-end.js
+++ app/states/level-end.js
@@ -14,6 +14,11 @@
 
       this.restartText = game.add.text(this.game.world.centerX, 300, 'Tap to play again', { font: '16px Arial', fill: '#ffffff', align: 'left'});
       this.restartText.anchor.setTo(0.5, 0.5);
+
+      this.betaNotice = game.add.text(this.game.world.centerX, this.game.world.height - 20, 'This is a beta version\nLeave feedback and get updates here', { font: '16px Arial', fill: 'red', align: 'center'});
+      this.betaNotice.anchor.setTo(0.5, 0.5);
+      this.betaNotice.inputEnabled = true;
+      this.betaNotice.events.onInputDown.add(function(){window.location.href="http://codeperfectionist.com/portfolio/games/color-blindness-blitz/";});
     },
     update: function() {
       if(this.game.input.activePointer.justPressed()) {
