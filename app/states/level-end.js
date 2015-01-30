502cc8aedbec43473955fa8585e32927410e20c5
Jeff Schomay <jschomay@gmail.com>
2015-01-30 08:33:54 +0000

    add progress bar showing player's score out of max

--- app/states/level-end.js
+++ app/states/level-end.js
@@ -1,20 +1,27 @@
-  function LevelEnd() {}
+function LevelEnd() {}
 
-  LevelEnd.prototype = {
+LevelEnd.prototype = {
     preload: function() {
 
     },
     create: function() {
+      // level #
       var style = { font: 'bold 45px Arial', fill: '#ffffff', align: 'center'};
       this.titleText = this.game.add.text(this.game.world.centerX, 100, 'Level '+this.game.pacing.level+'\nFinished', style);
       this.titleText.anchor.setTo(0.5, 0.5);
 
-      this.scoreText = game.add.text(this.game.world.centerX, 200, 'Score: '+this.game.score.levelScore, { font: '16px Arial', fill: '#ffffff', align: 'left'});
+      // score
+      this.scoreText = game.add.text(this.game.world.centerX, 200, 'Score: 0', { font: '16px Arial', fill: '#ffffff', align: 'left'});
       this.scoreText.anchor.setTo(0.5, 0.5);
 
+      // next level
       this.restartText = game.add.text(this.game.world.centerX, 300, 'Tap to play again', { font: '16px Arial', fill: '#ffffff', align: 'left'});
       this.restartText.anchor.setTo(0.5, 0.5);
 
+      // progress bar
+      this.drawProgressBar();
+
+      // beta notice
       this.betaNotice = game.add.text(this.game.world.centerX, this.game.world.height - 20, 'This is a beta version\nLeave feedback and get updates here', { font: '16px Arial', fill: 'red', align: 'center'});
       this.betaNotice.anchor.setTo(0.5, 0.5);
       this.betaNotice.inputEnabled = true;
@@ -28,4 +35,44 @@
     }
   };
 
-  module.exports = LevelEnd
+LevelEnd.prototype.drawProgressBar = function(){
+  console.log("you scored", this.game.score.levelScore, 'out of', this.game.score.maxLevelScore);
+
+  var percentage;
+  if(this.game.score.levelScore > this.game.score.maxLevelScore) {
+    // could happen with bonuses
+    percentage = 1;
+  } else {
+    percentage = this.game.score.levelScore / this.game.score.maxLevelScore;
+  }
+
+  var position = {x: this.game.width * 0.1, y: 250};
+  var color = 0xffd900;
+
+  var bestScore = game.add.graphics(position.x, position.y);
+  // set a fill and line style
+  bestScore.beginFill();
+  bestScore.lineStyle(20, color, 0.5);
+  // draw best possible score
+  bestScore.moveTo(0, 0);
+  bestScore.lineTo(this.game.width * 0.8, 0);
+  bestScore.endFill();
+
+  var playerScore = game.add.graphics(position.x, position.y);
+  // set a fill and line style
+  playerScore.beginFill();
+  playerScore.lineStyle(20, color, 1);
+  // draw player's score
+  playerScore.moveTo(0, 0);
+  playerScore.lineTo(percentage * this.game.width * 0.8, 0);
+  playerScore.endFill();
+
+
+  playerScore.scale.x = 0;
+  game.add.tween(playerScore.scale).to({x: 1}, 3500, Phaser.Easing.Quadratic.Out, true)
+    .onUpdateCallback(function(){
+        this.scoreText.text =  'Score: ' + Math.round(this.game.score.levelScore * playerScore.scale.x);
+    }, this);
+};
+
+module.exports = LevelEnd;
