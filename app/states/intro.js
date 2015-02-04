d4c0ca8a1707b42e2789d15b91b98366cb46f1ae
Jeff Schomay <jschomay@gmail.com>
2015-02-04 20:10:44 +0000

    learned how to wrap text, no need for "\n"
    except for where I want to manually new line

--- app/states/intro.js
+++ app/states/intro.js
@@ -11,7 +11,7 @@
       this.titleText = this.game.add.text(this.game.world.centerX, 100, 'Color\nBlindness\nBlitz', style);
       this.titleText.anchor.setTo(0.5, 0.5);
 
-      this.instructionsText = this.game.add.text(this.game.world.centerX, 300, 'Rules:  Tap a word that matches the\ncolor of the lit up word.  If you get\nit right in time, the word will clear.\n\nTry to clear all the words.\n\nTap anywhere to start.', { font: '16px Arial', fill: '#ffffff', align: 'left'});
+      this.instructionsText = this.game.add.text(this.game.world.centerX, 300, 'Rules:  Tap a word that matches the color of the lit up word.  If you get it right in time, the word will clear.\n\nTry to clear all the words.\n\nTap anywhere to start.', { font: '16px Arial', fill: '#ffffff', align: 'left', wordWrap: true, wordWrapWidth: this.game.width * 0.8});
       this.instructionsText.anchor.setTo(0.5, 0.5);
 
       this.betaNotice = this.game.add.text(this.game.world.centerX, this.game.world.height - 20, 'This is a beta version\nLeave feedback and get updates here', { font: '16px Arial', fill: 'red', align: 'center'});
