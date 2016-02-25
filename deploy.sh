155658da54a09270d9ad6d179b878777c221b9f3
Jeff Schomay <jschomay@gmail.com>
2016-02-25 08:15:55 -0700

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 
 git checkout gh-pages
 git merge master
+rm -rf public/*
 node_modules/brunch/bin/brunch build
+git add .
 git commit . -m "compile public/"
 git subtree push --prefix public origin gh-pages
 git checkout master
